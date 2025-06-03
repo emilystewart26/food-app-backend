db1d2336e718c7bb757f5d11e2b40ee70424f869
EKomin220 <ifa.komin@gmail.com>
2025-06-03 16:29:53 +0100

    first draft schemas

---
+++ Models/Review.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose")
+
+const reviewSchema = new mongoose.Schema({
+    foodReview: {
+        type: String,
+        required: true,
+    },
+    foodStars: {
+        type: Number,
+        required: true,
+    },
+    ambianceReview: {
+        type: String,
+        required: true,
+    },
+    ambianceStars: {
+        type: Number,
+        required: true,
+    },
+    serviceReview: {
+        type: String,
+        required: true,
+    },
+    serviceStars: {
+        type: Number,
+        required: true,
+    },
+    locationReview: {
+        type: String,
+        required: true,
+    },
+    locationStars: {
+        type: Number,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    restaurantID:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Restaurant",
+        required: true,
+    },
+})
+
+module.exports = mongoose.model("Review", reviewSchema)
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-1. clone repo
-2. npm init -y
-3. npm i nodemon express mongoose dotenv cors
-4. create .env file >> add MongoDB connection string
+1. Clone repo: git clone https://github.com/emilystewart26/food-app-backend.git
+2. Initialise npm: npm init -y
+3. Install dependencies: npm i nodemon express mongoose dotenv cors bcrypt
+4. Create .env file locally & add MongoDB connection string
