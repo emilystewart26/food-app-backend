b758a347cb1285b3984ac894b82257b14f15a4f0
EKomin220 <ifa.komin@gmail.com>
2025-06-10 17:13:30 +0100

    geolocation and nearby search capability added

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -2,7 +2,6 @@ const express = require("express")
 const router = express.Router()
 const { 
     getRestaurants,
-    //getRestaurantsByLocation, 
     getRestaurantsByCity,
     getRestaurantsByName,
     getRestaurantById,
@@ -10,17 +9,24 @@ const {
     addRestaurant,
     updateRestaurant,
     deleteRestaurant,
+    getRestaurantsWithin1km,
+    getRestaurantsWithin3km,
+    getRestaurantsWithin5km,
 } = require("../controllers/restaurantControllers")  
 const {authenticateToken, authorizeRole}  = require("../middleware/authMiddleware");
 
 
+
 // Get all restaurants + get restaurants by different search criteria (anyone - no login required)
 router.get("/", getRestaurants);
+router.get("/nearby", getRestaurantsWithin1km);
+router.get("/nearby", getRestaurantsWithin3km);
+router.get("/nearby", getRestaurantsWithin5km);
 router.get("/:id", getRestaurantById);
 router.get("/userid/:userId", getRestaurantsByUserId); // userId = "vendor" user._id
 router.get("/name/:name", getRestaurantsByName);
 router.get("/city/:city", getRestaurantsByCity);
-//router.get("/location/:location", getRestaurantsByLocation); // !!! investigate the logic - e.g using coordinates+search radius ???
+
 
 // Create restaurant (if logged in as "vendor")
 router.post("/", authenticateToken, authorizeRole("vendor"), addRestaurant);
